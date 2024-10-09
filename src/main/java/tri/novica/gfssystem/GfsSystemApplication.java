ee1be086f46d46f6e8e8217bceb110a032f38253
Novke <novke.brate@gmail.com>
2024-10-09 12:27:28 +0200

    spring init

---
+++ src/main/java/tri/novica/gfssystem/GfsSystemApplication.java
@@ -0,0 +1,13 @@
+package tri.novica.gfssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GfsSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tri/novica/gfssystem/GfsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tri.novica.gfssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
