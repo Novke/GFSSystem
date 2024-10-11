9fb51d2d240a907f795dab28f5aa9e3c4325057b
Novke <novke.brate@gmail.com>
2024-10-11 10:26:58 +0200

    Grupa findById Fetch Students

--- src/main/java/tri/novica/gfssystem/repository/GrupaRepository.java
+++ src/main/java/tri/novica/gfssystem/repository/GrupaRepository.java
@@ -1,9 +1,17 @@
 package tri.novica.gfssystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tri.novica.gfssystem.entity.Grupa;
 
+import java.util.Optional;
+
 @Repository
 public interface GrupaRepository extends JpaRepository<Grupa, Long> {
+
+    @Query("SELECT g FROM Grupa g LEFT JOIN fetch g.studenti where g.id = :id")
+    Optional<Grupa> findByIdFetchStudents(@Param("id") Long id);
+
 }
