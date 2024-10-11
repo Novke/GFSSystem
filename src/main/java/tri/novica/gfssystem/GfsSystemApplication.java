4093a3f78b5a9f3d7613eca0812a9d8e54a74d15
Novke <novke.brate@gmail.com>
2024-10-11 09:17:58 +0200

    ModelMapper

--- src/main/java/tri/novica/gfssystem/GfsSystemApplication.java
+++ src/main/java/tri/novica/gfssystem/GfsSystemApplication.java
@@ -1,6 +1,9 @@
 package tri.novica.gfssystem;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,5 +12,13 @@ public class GfsSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(GfsSystemApplication.class, args);
     }
+
+    @Bean
+    ModelMapper mapper(){
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return mapper;
+    }
+
 
 }
