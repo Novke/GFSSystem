d80f6ac26dd31649f9a38509f26f07d6c07e55f4
Novke <novke.brate@gmail.com>
2024-10-17 09:54:07 +0200

    index2int je silent

--- src/main/java/tri/novica/gfssystem/utility/Utility.java
+++ src/main/java/tri/novica/gfssystem/utility/Utility.java
@@ -1,8 +1,11 @@
 package tri.novica.gfssystem.utility;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 public class Utility {
     public static int index2int(String index) {
         Pattern pattern = Pattern.compile("\\d+$");  // "\\d+" matches digits, "$" ensures it's at the end of the string
@@ -12,7 +15,9 @@ public class Utility {
             return Integer.parseInt(matcher.group());
         } else {
             // Handle the case where no number is found
-            throw new IllegalArgumentException("No number found in the input string: " + index);
+//            throw new IllegalArgumentException("No number found in the input string: " + index);
+            log.error("No number found in index: " + index);
+            return Integer.MAX_VALUE;
         }
     }
 }
