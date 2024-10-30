111994a4de17cb8f96723a7bded8caccb299252b
Novke <novke.brate@gmail.com>
2024-10-30 17:13:38 +0100

    cors za custom url

--- src/main/java/tri/novica/gfssystem/security/CorsConfig.java
+++ src/main/java/tri/novica/gfssystem/security/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Objects;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
@@ -14,9 +16,13 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all routes
-                .allowedOrigins(environment.getProperty("gfs.front.url")) // Allow specific origin (your Angular app)
+                .allowedOrigins(getOrigins()) // Allow specific origin (your Angular app)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // Allow specific methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials if needed (cookies, auth, etc.)
+    }
+
+    private String[] getOrigins(){
+        return Objects.requireNonNull(environment.getProperty("gfs.front.url")).split(", ");
     }
 }
