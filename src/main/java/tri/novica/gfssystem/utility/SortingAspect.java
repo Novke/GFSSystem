20b532ec8da895afb806eade03db37ccd0c94117
Novke <novke.brate@gmail.com>
2024-11-21 23:49:21 +0100

    SortingAspect -> TestDetails

--- src/main/java/tri/novica/gfssystem/utility/SortingAspect.java
+++ src/main/java/tri/novica/gfssystem/utility/SortingAspect.java
@@ -6,6 +6,11 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import tri.novica.gfssystem.dto.predavanje.PredavanjeDetails;
+import tri.novica.gfssystem.dto.test.TestDetails;
+
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.stream.Collectors;
 
 import static tri.novica.gfssystem.utility.Utility.index2int;
 
@@ -20,8 +25,20 @@ public class SortingAspect {
     public void sortPredavanja(JoinPoint jp, PredavanjeDetails predavanje){
         predavanje.getAktivnosti()
                 .sort(
-                        (a1, a2) -> index2int(a1.getStudent().getIndeks())-index2int(a2.getStudent().getIndeks())
+                        Comparator.comparingInt(a -> index2int(a.getStudent().getIndeks()))
                 );
+    }
+
+    @Pointcut("execution(public tri.novica.gfssystem.dto.test.TestDetails tri.novica.gfssystem.service.TestService.*(..))")
+    public void vracenoTestDetails(){}
+
+    @AfterReturning(pointcut = "vracenoTestDetails()", returning = "test")
+    public void sortTest(JoinPoint jp, TestDetails test){
+        test.setPolaganja(test.getPolaganja()
+                .stream()
+                .sorted(
+                        Comparator.comparingInt(p -> index2int(p.getStudent().getIndeks()))
+                ).collect(Collectors.toCollection(LinkedHashSet::new)));
     }
 
 }
